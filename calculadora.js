e212d0c43e2c02d5af0a28ca1c72fa12af1781c4
Leonardo <leonardofreire.si@gmail.com>
2023-02-17 18:59:46 -0400

    commit

---
+++ calculadora.js
@@ -0,0 +1,21 @@
+const calculadora = {
+    soma (num1, num2){
+        return num1 + num2;
+    },
+
+    subtracao (num1, num2){
+        return num1 - num2;
+    },
+
+    divisao (num1, num2){
+        return num1 / num2;
+    },
+
+    multiplicacao (num1, num2){
+        return num1 * num2;
+    }
+}
+
+module.exports = {
+    calculadora
+};
